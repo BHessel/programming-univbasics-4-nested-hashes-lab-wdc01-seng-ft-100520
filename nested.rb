8bbd39a1dbe714e70d7a13aa5d9b899ad8d41eb4
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 17:28:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash = 
- 	programmer_hash{:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
-}
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -86,6 +82,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto] => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 programmer_hash
 end
 
